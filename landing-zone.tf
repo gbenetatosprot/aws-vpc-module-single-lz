0b727bd277dad98b2d428ec80269d403fa32836c
you@example.com <you@example.com>
2025-06-13 07:32:44 -0700

    add-second-vpc

--- landing-zone.tf
+++ landing-zone.tf
@@ -29,3 +29,31 @@ module "vpc1" {
 
   create_staging_subnet_route_table = true
 }
+
+module "vpc2" {
+  source  = "./modules/vpc-creation-protera"
+
+  name            = "bene-vpc-2"
+  cidr            = "192.168.50.0/23"
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["192.168.50.0/25", "192.168.51.0/25"]
+  public_subnets  = ["192.168.50.128/26", "192.168.51.128/26"]
+  staging_subnets = ["192.168.50.192/27"]
+
+  private_subnet_names = ["Private-AZ1", "Private-AZ2"]
+  public_subnet_names = ["Public-AZ1", "Public-AZ2"]
+  staging_subnet_names = ["Staging-Subnet-AZ1-PRV"]
+
+  enable_nat_gateway = false
+
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tgw_share = true
+
+  ram_principals = [503659244423]
+  ram_share_arn = "arn:aws:ram:us-east-1:706210432878:resource-share/2bc59596-8389-403c-af75-7f15d4351770"
+
+  create_staging_subnet_route_table = true
+}
