005bc29d877b44f09fb710fb340a92b9fe81b85b
you@example.com <you@example.com>
2025-06-13 06:29:07 -0700

    save-new

---
+++ landing-zone.tf
@@ -0,0 +1,26 @@
+################################################################################
+# VPC Creation - SPOKE
+################################################################################
+
+module "vpc1" {
+  source  = "./modules/vpc-creation-protera"
+
+  name            = "bene-vpc-1"
+  cidr            = "10.160.10.0/23"
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.160.10.0/25", "10.160.11.0/25"]
+  public_subnets  = ["10.160.10.128/26", "10.160.11.128/26"]
+  staging_subnets = ["10.160.10.192/27"]
+
+  private_subnet_names = ["Private-AZ1", "Private-AZ2"]
+  public_subnet_names = ["Public-AZ1", "Public-AZ2"]
+  staging_subnet_names = ["Staging-Subnet-AZ1-PRV"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  create_staging_subnet_route_table = true
+}
---
+++ modules/vpc-creation-protera/locals.tf
@@ -0,0 +1,17 @@
+locals {
+  len_public_subnets      = max(length(var.public_subnets), length(var.public_subnet_ipv6_prefixes))
+  len_private_subnets     = max(length(var.private_subnets), length(var.private_subnet_ipv6_prefixes))
+  len_staging_subnets    = max(length(var.database_subnets), length(var.database_subnet_ipv6_prefixes))
+
+  max_subnet_length = max(
+    local.len_private_subnets,
+    local.len_public_subnets,
+    local.len_staging_subnets
+
+  )
+
+  # Use `local.vpc_id` to give a hint to Terraform that subnets should be deleted before secondary CIDR blocks can be free!
+  vpc_id = try(aws_vpc_ipv4_cidr_block_association.this[0].vpc_id, aws_vpc.this[0].id, "")
+
+  create_vpc = var.create_vpc && var.putin_khuylo
+}
