483b9c39e141bfc3a4ac686f9e28ee8af3f09384
you@example.com <you@example.com>
2025-06-16 02:42:00 -0700

    add atachment creation

--- modules/vpc-creation-protera/tgw-acceptance.tf
+++ modules/vpc-creation-protera/tgw-acceptance.tf
@@ -18,3 +18,28 @@ module "tgw_peer" {
   ram_principals                        = var.ram_principals
 
 }
+
+data "aws_ec2_transit_gateway" "shared" {
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+  depends_on = [aws_route_table_association.private]
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-spoke" {
+  count = (
+    var.attachment_creation &&
+    try(length(data.aws_ec2_transit_gateway.shared.id) > 0, false)
+  ) ? 1 : 0
+
+  subnet_ids                                            = aws_subnet.private[*].id
+  transit_gateway_id                                    = data.aws_ec2_transit_gateway.shared.id
+  transit_gateway_default_route_table_association       = false
+  transit_gateway_default_route_table_propagation       = false
+  vpc_id                                                = aws_vpc.this[0].id
+
+  tags = {
+    Name = "securityVPC-Attach"
+  }
+}
